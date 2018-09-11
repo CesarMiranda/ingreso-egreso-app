3213069c68c7d533a2b8179f03f301ba0e830d7c
CesarMiranda <windblacksot@gmail.com>
2018-09-11 15:47:13 -0500

    Sección 8 lista - inicio redux

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,20 +9,42 @@ import { map } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { User } from './user.model';
 
+import { Store } from '@ngrx/store';
+import { AppState } from './../app.reducer';
+import { ActivarLoadingAction, DesActivarLoadingAction } from './../shared/ui.actions';
+import { SetUserAction } from './auth.actions';
+import { Subscription } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth, private router: Router, private afDB: AngularFirestore) { }
+  private userSubscription: Subscription = new Subscription();
+
+  constructor(private afAuth: AngularFireAuth, private router: Router, private afDB: AngularFirestore, private store: Store<AppState>) { }
 
   initAuthListener() {
-    this.afAuth.authState.subscribe((fbUser: firebase.User) => {
-      console.log(fbUser);
+
+    this.userSubscription = this.afAuth.authState.subscribe((fbUser: firebase.User) => {
+      // console.log(fbUser);
+      if (fbUser) {
+        this.afDB.doc(`${ fbUser.uid }/usuario`)
+          .valueChanges()
+          .subscribe( (usuarioObj: any) => {
+
+            const newUser = new User(usuarioObj);
+            this.store.dispatch(new SetUserAction(newUser));
+          } );
+      } else {
+        this.userSubscription.unsubscribe();
+      }
     });
   }
 
   crearUsuario(email: string, password: string, nombre: string, ) {
+    this.store.dispatch(new ActivarLoadingAction());
+
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
     .then(usuario => {
       // console.log(usuario);
@@ -35,24 +57,31 @@ export class AuthService {
       this.afDB.doc(`${ user.uid }/usuario`)
         .set(user)
         .then( () => {
+          this.store.dispatch(new DesActivarLoadingAction());
           this.router.navigate(['/']);
         });
 
     })
     .catch(error => {
-      console.error(error);
+      // console.error(error);
+      this.store.dispatch(new DesActivarLoadingAction());
       Swal('Error en el login', error.message, 'error');
     });
   }
 
   loginUsuario(email: string, password: string) {
+
+    this.store.dispatch( new ActivarLoadingAction() );
+
     this.afAuth.auth.signInWithEmailAndPassword(email, password)
       .then(resp => {
         // console.log(resp);
+        this.store.dispatch(new DesActivarLoadingAction());
         this.router.navigate(['/']);
       })
       .catch(error => {
         console.log(error);
+        this.store.dispatch(new DesActivarLoadingAction());
         Swal('Error en el login', error.message, 'error');
       });
   }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import { AppState } from './../../app.reducer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styles: []
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor(public _authService: AuthService, private router: Router) { }
+  cargando: boolean;
+  subscription: Subscription;
+
+  constructor(public _authService: AuthService, private router: Router, private store: Store<AppState>) { }
 
   ngOnInit() {
+
+    this.subscription = this.store.select('ui')
+      .subscribe( ui => this.cargando = ui.isLoading );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onLogin(data: any) {
-    console.log(data);
+    // console.log(data);
     this._authService.loginUsuario(data.email, data.password);
   }
 
